94c5cc2a50f1bf3b4dd035ea4cbc59d72be898c4
GodProgrammerMan <960842214@qq.com>
2019-09-11 17:10:49 +0800

    msg

--- src/main/java/com/sz/water/MainApplication.java
+++ src/main/java/com/sz/water/MainApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
 
-	// 主启动类
+	// 主启
 	public static void main(String[] args) {
 		System.out.println("启动...");
 		SpringApplication.run(MainApplication.class, args);
