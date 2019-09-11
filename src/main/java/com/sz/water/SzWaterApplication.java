698bbd923367c675999016c99abe9182158fbdd6
GodProgrammerMan <960842214@qq.com>
2019-09-11 16:40:45 +0800

    2019-9-11

---
+++ src/main/java/com/sz/water/SzWaterApplication.java
@@ -0,0 +1,13 @@
+package com.sz.water;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzWaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzWaterApplication.class, args);
+	}
+
+}
